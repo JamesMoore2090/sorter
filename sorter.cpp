ddcb5ab515eeda6c1efb7ac2ace595c685030e5b
James Moore <jmoore7@rosemary.umw.edu>
2014-11-21 23:31:50 -0500

    random numbers are working. So is bubble sort!!!

--- sorter.cpp
+++ sorter.cpp
@@ -14,6 +14,9 @@
 #include <string>
 #include <cstdlib>
 
+
+bool isSorted(int [], int);
+void bubbleSort(int [], int);
 using namespace std;
 int main(int argc, char* argv[]){
 	// basic bailout if there are anything else than 3 arguments
@@ -33,15 +36,17 @@ int main(int argc, char* argv[]){
 	}//end if
 	int unsortedArray[arraySize];
 	for(int i= 0; i < arraySize; i++){
-		int randomNumber;
+		int randomNum = rand() %999999;
 		unsortedArray[i] = randomNum;
+	cout << unsortedArray[i] << ", ";
 	}//end for
-
+	
 
 
 	string whatSort = argv[1];
 	if((whatSort == "bubble") || (whatSort == "Bubble")){
 		cout << "You have entered " << whatSort << " 3" << endl;
+		bubbleSort(unsortedArray, arraySize);
 		//return 1;
 	}// end if
 	else if((whatSort == "bucket") || (whatSort == "Bucket")){
@@ -54,11 +59,43 @@ int main(int argc, char* argv[]){
 	}// end if
 	else cout << "You have entered an invalid type of sort" << endl;
 
+	if(isSorted(unsortedArray, arraySize)){
+		cout << "the array is sorted" << endl;
+		for(int i =0; i<arraySize; i++){
+			cout << unsortedArray[i] << endl;
+		}
+	}// end if
 
-
+	if(!isSorted(unsortedArray, arraySize)){
+		cout << "the array is not sorted" << endl;
+	}// end if
 
 
 
 
 
 }// end the main function
+
+//this function is used to check to see if the list is sorted.
+bool isSorted(int *array, int size){
+	for(int i = 0; i<size-1; i++){
+		if(array[i] > array[i+1]){
+			return false;
+		}// end if
+	}// end for
+	return true;
+}// end function
+
+// THis is the bubble sort function
+void bubbleSort(int *array, int size){
+	int temp; // used for the swap
+	for(int i = size; i >1; i--){
+		for(int j =0; j < i-1; j++){
+			if(array[j]>array[j+1]){
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}// end if
+		}// end for j
+	}// end for i
+}// end function
